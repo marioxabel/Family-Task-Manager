8a8affa7bf49db063a78d18628bb20bab0554eb6
gabipayan <gapayan26@gmail.com>
2024-09-26 21:16:26 -0600

    user authentication

--- server/models/user.js
+++ server/models/user.js
@@ -1,9 +1,19 @@
 import { Model, DataTypes } from 'sequelize';
+import bcrypt from 'bcrypt';  // Import bcrypt for hashing passwords
 import sequelize from '../config/connection.js';  // Import sequelize instance
 
 class User extends Model {}
+// Check the password against the hashed password
+checkPassword(loginPw) {
+  return bcrypt.compareSync(loginPw, this.password);
+};
 
 User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true
+  },
   email: {
     type: DataTypes.STRING,
     allowNull: false,
@@ -22,15 +32,47 @@ User.init({
   },
   parent_id: {
     type: DataTypes.INTEGER,
-    allowNull: true  // Null for parent users
+    allowNull: true,  // Null for parent users
+    references: {
+      model: 'users', // Self-reference for parent-child relationship
+        key: 'id'
+    }
   }
 }, {
   sequelize,
   modelName: 'User',
   tableName: 'users',
   schema: 'public',
-  timestamps: true  // Includes createdAt and updatedAt
+  timestamps: true,  // Includes createdAt and updatedAt
+  hooks: {
+      // Hash the password before saving
+      async beforeCreate(newUser) {
+        newUser.password = await bcrypt.hash(newUser.password, 10);
+      },
+      async beforeUpdate(updatedUser) {
+        if (updatedUser.password) {
+          updatedUser.password = await bcrypt.hash(updatedUser.password, 10);
+        }
+      }
+    }
 });
+
+
+// Define associations
+User.associate = (models) => {
+  // A parent user can have multiple child users
+  User.hasMany(models.User, {
+    as: 'children',
+    foreignKey: 'parent_id',
+    onDelete: 'CASCADE'
+  });
+
+  // A parent can have multiple notifications
+  User.hasMany(models.Notification, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+};
 
 export default User
 
