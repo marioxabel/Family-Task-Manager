8ea93f9a1d1f137f26bbb2cac08596b62dfc7deb
Mario <31260739+marioxabel@users.noreply.github.com>
2024-09-29 23:23:09 -0600

    child routes

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,8 +1,11 @@
 import { Router } from 'express';
 import { parentRouter } from './parent-routes.js';
+import { childRouter } from './child-routes.js';
+
 
 const router = Router();
 
 router.use('/parents', parentRouter);
+router.use('/children', childRouter);
 
 export default router;
