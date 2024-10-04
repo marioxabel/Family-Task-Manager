11e9ad699738b0cb028b66b819f71ce244e68b03
Mario <31260739+marioxabel@users.noreply.github.com>
2024-10-04 09:55:58 -0600

    app.jsx clean

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import Navbar from './components/Navbar';
 import { Outlet } from 'react-router-dom';
 import './App.css';  
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
